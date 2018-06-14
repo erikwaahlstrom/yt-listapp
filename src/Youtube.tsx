01fe1be47eba8f70c8c0ee709e943af8062ad428
erikwaahlstrom <erik93_93@hotmail.com>
2018-06-14 15:52:48 +0200

    call to api working

--- src/Youtube.tsx
+++ src/Youtube.tsx
@@ -1,15 +1,51 @@
 import * as React from 'react';
 
+const URL = 'https://www.googleapis.com/youtube/v3/search';
+const KEY = 'AIzaSyB2gmaBHYLim0wrHh8xsB93yvTriLJHIjY';
+const channelID = 'UCp8lM3X0hpFDVlaFuZBvw0g';
+const maxRes = 10;
+
+// const result = 10;
+
+const finalURL = `${URL}?key=${KEY}&channelId=${channelID}&part=snippet,id&order=date&maxResults=${maxRes}`;
+
 class Youtube extends React.Component {
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            resultyt: []
+        };
+        this.clicked = this.clicked.bind(this);
+    }
+
+    // Fetch youtube api
+    public clicked() {
+        fetch(finalURL)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch((error) => {
+                return console.error(error);
+            })
+    }
+
     public render() {
-        return (<div>
-            <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/c6t3bW7kx6E"
-            frameBorder="0"
-            allowFullScreen />
-        </div>)
+        console.log(finalURL);
+        return (
+        <div>
+            <button onClick={this.clicked}>Get youtube videos</button>
+
+            <div>
+                <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/c6t3bW7kx6E"
+                frameBorder="0"
+                allowFullScreen={true} />
+            </div>
+        </div>
+        )
     }
 }
 
